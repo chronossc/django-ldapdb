ddce93dfb8683086a09e4d64b4f946ea91af4fed
jlaine <jlaine@e071eeec-0327-468d-9b6a-08194a12b294>
2011-04-11 14:10:19 +0000

    partially resync settings with django defaults
    git-svn-id: https://svn.bolloretelecom.eu/opensource/django-ldapdb/trunk/@1044 e071eeec-0327-468d-9b6a-08194a12b294

--- settings.py
+++ settings.py
@@ -19,6 +19,8 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
 TIME_ZONE = 'America/Chicago'
@@ -33,7 +35,11 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
-# Absolute path to the directory that holds media.
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = ''
 
