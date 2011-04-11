4cbdd6aa3544b7c66943f7223a88f7956940c261
jlaine <jlaine@e071eeec-0327-468d-9b6a-08194a12b294>
2011-04-11 15:46:30 +0000

    remove _collect_sub_objects() hack
    git-svn-id: https://svn.bolloretelecom.eu/opensource/django-ldapdb/trunk/@1048 e071eeec-0327-468d-9b6a-08194a12b294

--- ldapdb/models/base.py
+++ ldapdb/models/base.py
@@ -56,14 +56,6 @@ class Model(django.db.models.base.Model):
         super(Model, self).__init__(*args, **kwargs)
         self.saved_pk = self.pk
 
-    def _collect_sub_objects(self, seen_objs, parent=None, nullable=False):
-        """
-        This private API seems to be called by the admin interface in django 1.2
-        """
-        pk_val = self._get_pk_val()
-        seen_objs.add(self.__class__, pk_val, self,
-                      type(parent), parent, nullable)
-
     def build_rdn(self):
         """
         Build the Relative Distinguished Name for this entry.
